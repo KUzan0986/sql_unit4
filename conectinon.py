e3e5245c1ad56a180617ea33a78290164204b579
Pavel Gaponenko <Pavel.gap666@gmail.com>
2021-11-28 14:00:28 +0500

    added new selects

--- conectinon.py
+++ conectinon.py
@@ -39,6 +39,16 @@ class Conection:
         print("4 - названия сборников, вышедших в период с 2018 по 2020 год включительно")
         print("5 - исполнители, чье имя состоит из 1 слова")
         print('6 - название треков, которые содержат слово "мой"/"my"')
+        print('7 - количество исполнителей в каждом жанре')
+        print('8 - количество треков, вошедших в альбомы 2019-2020 годов')
+        print('9 - средняя продолжительность треков по каждому альбому')
+        print('10 - все исполнители, которые не выпустили альбомы в 2020 году')
+        #print('11 - названия сборников, в которых присутствует конкретный исполнитель (выберите сами)')
+        #print('12 - название альбомов, в которых присутствуют исполнители более 1 жанра')
+        #print('13 - наименование треков, которые не входят в сборники')
+        #print('14 - исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько)')
+        #print('15 - название альбомов, содержащих наименьшее количество треков')
+
         while True:
             сheck = input("введите число")
             match сheck:
@@ -60,10 +70,36 @@ class Conection:
                 case "6":
                     path = os.getcwd() + "\\select\\select_my"
                     break
+                case "7":
+                    path = os.getcwd() + "\\select\\select_count_group"
+                    break
+                case "8":
+                    path = os.getcwd() + "\\select\\select_songs_in_alboms_year_19_20"
+                    break
+                case "9":
+                    path = os.getcwd() + "\\select\\select_avg_lasting"
+                    break
+                case "10":
+                    path = os.getcwd() + "\\select\\select_realised_not_in_2020"
+                    break
+                case "11":
+                    path = os.getcwd() + "\\select\\select_count_group"
+                    break
+                case "12":
+                    path = os.getcwd() + "\\select\\select_count_group"
+                    break
+                case "13":
+                    path = os.getcwd() + "\\select\\select_count_group"
+                    break
+                case "14":
+                    path = os.getcwd() + "\\select\\select_count_group"
+                    break
+                case "15":
+                    path = os.getcwd() + "\\select\\select_count_group"
+                    break
                 case _:
                     print("Вы ввели не то!")
 
         stri_res = _file_reader(path)
         result = self.connection.execute(stri_res).fetchall()
         pprint(result)
-
